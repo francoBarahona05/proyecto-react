ed32d05bb6b3dc646a1e30f2d9cc5c176d95655f
franco barahona <franco.barahona.07@gmail.com>
2022-12-04 00:22:57 -0300

    entrega final

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -8,58 +8,41 @@ export default function Checkout () {
   const [email, setEmail] = useState("");
   const [tel , setTel] = useState("");
   const [orderId , setOrderId] = useState("")
+
+  // const validateEmail = (email,name , tel)=>{
+  //   if(!email.includes("@",".com"))
+  // return "el email debe tener `@` `.com`" 
+  // if (name && tel == "")
+  //   return " no se puede dejar el campo vacio"
+  //   else{
+  //     validar()
+  //   }
+  // }
+
 function validar() {
   const ticket = {
     bayer:{name , tel , email},
     Cart,
-    totalPrice
-    
+    totalPrice 
   }
-
-  // if (eval.target|| ticket.bayer.email || ticket.bayer.tel == "") {
-  //   alert("llenar todos los datos para poder terminar la compra")
-  // }
-
-
-    // const login = (name , tel , email)=>{
-    
-        const db = getFirestore();
-        const orders = collection(db , "orders")
-        addDoc(orders , ticket).then(({id})=>{
-          setOrderId(id)
-        })
-      
-    // }
-
-    const validateEmail = (email,name , tel)=>{
-      if(!email.includes("@",".com"))
-    return "el email debe tener `@` `.com`" 
-    if (name == "")
-      return " no se puede dejar el campo vacio"
-    
-    if (tel == "") 
-      return "no se puede dejar el campo vacio"
-    
-  }
-  const errorM = validateEmail()
-
+  const db = getFirestore();
+  const orders = collection(db , "orders")
+  addDoc(orders , ticket).then(({id})=>{
+    setOrderId(id)
+  })
 }
-
   return (
   <>
     {orderId  ? ("gracias por confiar en nosotros , tu orden de compra es : " + orderId ) :
     (
     <div>
-      <form onSubmit={ev=>{
-        ev.preventDefault()
-        
-      }} > 
+      
       <h1>ingrese datos</h1>
-      <input type="text" placeholder='name' value={name}  onChange={(e)=>setName(e.target.value)}  />
-      <input type="tel" placeholder='tel' value={tel} onChange={(e)=>setTel(e.target.value)}/>
-      <input type="email" placeholder='email' value={email} onChange={(e)=>setEmail(e.target.value)} />
-      <button disabled={errorM} onClick={validar}>terminar</button>
-      </form>
+      <input type="text" placeholder='names'   onChange={(e)=>setName(e.target.value)}  />
+      <input type="tel" placeholder='tell'  onChange={(e)=>setTel(e.target.value)}/>
+      <input type="email" placeholder='email'  onChange={(e)=>setEmail(e.target.value)} />
+      <button  onClick={validar}>terminar</button>
+    
     </div>
     )
     }
